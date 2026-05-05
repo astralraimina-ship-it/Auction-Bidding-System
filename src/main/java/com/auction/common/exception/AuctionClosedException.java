5fffa08f748536d8a8c99153e969f9c1018c2caa
astralraimina-ship-it <astralraimina@gmail.com>
2026-05-05 23:27:02 +0700

    xoa file khong can thiet + them anti snipe

--- src/main/java/com/auction/common/exception/AuctionClosedException.java
+++
@@ -1,8 +0,0 @@
-package com.auction.common.exception;
-
-public class AuctionClosedException extends Exception{
-    @Override
-    public String getMessage() {
-        return "This auction is closed";
-    }
-}
